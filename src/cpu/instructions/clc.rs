d499ae6f6a315a1c417a80c580cbb59d71600905
ellist12 <ellist1212@gmail.com>
2026-03-02 20:20:27 +0700

    add missing CLC docs

--- src/cpu/instructions/clc.rs
+++ src/cpu/instructions/clc.rs
@@ -3,6 +3,12 @@ use crate::cpu::cpu::Cpu;
 pub struct CLC;
 
 impl CLC {
+    // CLC (Clear Carry)
+    // Set status carry di register status cpu menjadi 0
+    // Ukuran opcode : 2 byte
+    // Jumlah cycle : 2
+    // Contoh kode assembly : CLC [18]
+    // Artinya : Clear status carry di register status
     pub fn clear(cpu: &mut Cpu) -> u16 {
         cpu.status = cpu.status & 0b11111110;
         cpu.cycle += 2;
